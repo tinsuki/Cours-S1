cd584346d85eb24bdc1d479ce9b6adbeeb7ea0a6
tinsuki <83704875+tinsuki@users.noreply.github.com>
2023-01-03 15:22:00 +0100

    rien

---
+++ DEV/The_Game/functions.cpp
@@ -0,0 +1,10 @@
+#include "functions.h"
+
+void push(List* aList, Element* anElement){
+    anElement->next = aList->first;*
+    aList->first = *anElement;
+}
+
+int top(const List * aList){
+
+}
--- DEV/The_Game/main.cpp
+++ DEV/The_Game/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-
-using namespace std;
+#include "typeDef.h"
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    List hand;
     return 0;
 }
