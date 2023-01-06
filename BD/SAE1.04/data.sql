8dd3761927e145c77218a7e251ab71d9f3b99198
tinsuki <83704875+tinsuki@users.noreply.github.com>
2023-01-06 20:02:36 +0100

    SAE BD finie

--- BD/SAE1.04/data.sql
+++ BD/SAE1.04/data.sql
@@ -5,96 +5,93 @@
 -- date         : 20/11/22
 -- role         : insert un jeu de données pour l'explication du premier jalon 
 -- projet       : gestloc
--- resultat dans: data.out
 -- ==========================
 
-spool data.out;
 
 
--- Insertion de plusieurs Agences :
+-- Insertion de plusieurs Agences
 
--- Agence n°1 : 
-INSERT INTO AGENCE (idAgence, nomAgence, adrAgence, contactAgence)
-VALUES (1, 'AgenceLyon', '143 Rue Paul Bert, 69003 Lyon, France', '0478622215');
+INSERT INTO AGENCE (idAgence, nom, adresse, contact)
+VALUES (1, 'AgenceLyon', 'Lyon, France', 'AgenceImmoLyon@example.com');
 
+INSERT INTO AGENCE (idAgence, nom, adr, contact)
+VALUES (2, 'AgenceLille', 'Lille, France', 'AgenceImmoLille@example.com');
 
--- Agence n°2 :
+INSERT INTO AGENCE (idAgence, nom, adr, contact)
+VALUES (3, 'AgenceParis', 'Paris, France', 'AgenceImmoParis@example.com');
 
-INSERT INTO AGENCE (idAgence, nomAgence, adrAgence, contactAgence)
-VALUES (2, 'AgenceLille', '255 Rue Nationale, 59800 Lille, France', '0963643769');
+INSERT INTO AGENCE (idAgence, nom, adr, contact)
+VALUES (4, 'AgenceLaRochelle', 'LaRochelle, France', 'AgenceImmoLaRochelle@example.com');
 
-
-
--- Insertion de plusieurs proprietaires :
-
--- proprietaire n°1 :
-
-INSERT INTO PROPRIETAIRE (idProprietaire, nomProprietaire, prenomProprietaire, telProprietaire, mailProprietaire, ibanProprietaire)
-VALUES (10, 'MARTIN', 'Jean', '0666666666', 'MartinJean@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXX');
-
--- proprietaire n°2 :
-
-INSERT INTO PROPRIETAIRE (idProprietaire, nomProprietaire, prenomProprietaire, telProprietaire, mailProprietaire, ibanProprietaire)
-VALUES (20, 'Mary', 'Laetitia', '0677777777', 'MaryLaetitia@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXX');
-
--- proprietaire n°3 :
-
-INSERT INTO PROPRIETAIRE (idProprietaire, nomProprietaire, prenomProprietaire, telProprietaire, mailProprietaire, ibanProprietaire)
-VALUES (30, 'Lefort', 'ELisabeth', '0688888888', NULL, 'FRXXXXXXXXXXXXXXXXXXXXXXXXX');
+INSERT INTO AGENCE (idAgence, nom, adr, contact)
+VALUES (5, 'AgenceRochefort', 'Rochefort, France', 'AgenceImmoRochefort@example.com');
 
 
 
--- Insertion de plusieurs locataire :
+-- Insertion de Plusieurs Proprietaires*
 
--- Locataire n°1 :
+INSERT INTO PROPRIETAIRE (idPropri, nom, prenom, tel, mail, iban)
+VALUES (1, 'MARTIN', 'Jean', '0601010101', 'MartinJean@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXX01');
+
+INSERT INTO PROPRIETAIRE (idPropri, nom, prenom, tel, mail, iban)
+VALUES (2, 'MARTIN', 'Lucas', '0602020202', 'MartinJean@example.com', 'FRXXXXXXXXXXXXXXXXXXXXX03');
+
+INSERT INTO PROPRIETAIRE (idPropri, nom, prenom, tel, mail, iban)
+VALUES (3, 'MET', 'Delphine', '0603030303', 'MetDelphine@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXX04');
+
+INSERT INTO PROPRIETAIRE (idPropri, nom, prenom, tel, mail, iban)
+VALUES (4, 'MAZURIER', 'Yann', '0604040404', 'MazurierYann@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXX06');
+
+INSERT INTO PROPRIETAIRE (idPropri, nom, prenom, tel, mail, iban)
+VALUES (5, 'JEAN', 'Mark', '0605050505', 'MazurierYann@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXX07');
+
+-- Insertion de plusieurs Locataire
+
+INSERT INTO LOCATAIRE (id, nom, prenom, tel, mail, iban)
+VALUES (1, 'Muller', 'Lucie', '0610101010', 'MullerLucie@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXX02');
+
 INSERT INTO LOCATAIRE (idLocataire, nomLocataire, prenomLocataire, telLocataire, mailLocataire, ibanLocataire)
-VALUES (11, 'Muller', 'Lucie', '0611111111', 'MullerLucie@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+VALUES (2, 'SIMON', 'Laurent', '0620202020', 'SimonLaurent@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXX05');
 
--- Locataire n°2 :
 INSERT INTO LOCATAIRE (idLocataire, nomLocataire, prenomLocataire, telLocataire, mailLocataire, ibanLocataire)
-VALUES (22, 'Adam', 'Adele', '0622222222', 'AdamAdele@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+VALUES (3, 'MATHIEU', 'Leana', '0630303030', 'MathieuLeana@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXX05');
 
--- Locataire n°3 :
-INSERT INTO LOCATAIRE (idLocataire, nomLocataire, prenomLocataire, telLocataire, mailLocataire, ibanLocataire)
-VALUES (33, 'Girard', 'Olivier', '0633333333', 'GirardOlivier@example.com', 'FRXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+-- Insertion de plusieurs Syndics
 
-
--- Insertion de plusieurs syndics :
-
--- Syndic n°1 :
-INSERT INTO SYNDIC  (idSyndic, nomSyndic, prenomSyndic, contactSyndic)
+INSERT INTO SYNDIC  (id, nom, prenom, contact)
 VALUES (1, 'Guyot', 'Gilles', 'GuyotGilles@example.com');
 
--- Syndic n°2 : 
 INSERT INTO SYNDIC  (idSyndic, nomSyndic, prenomSyndic, contactSyndic)
-VALUES (2, 'Leroux', 'Pauline', 'LerouxPauline@example.com');
+VALUES (2, 'MARTIN', 'François', 'MartinFrançois@example.com');
 
+INSERT INTO SYNDIC  (idSyndic, nomSyndic, prenomSyndic, contactSyndic)
+VALUES (3, 'MARTIN', 'Lou', 'MartinLou@example.com');
 
+-- Insertion de plusieurs biens
 
--- Insertion plusieurs Bien :
+INSERT INTO BIEN (idBien, idAgence, idPropri, idSyndic, adresse, typeBail, Loyer, surface)
+Values (1,  2, 2, 2, 'Lyon, France', 3, 385, 25);
 
--- Bien n°1 :
-INSERT INTO BIEN (idBien, adrBien, bail, loyer, surface, nbrVisite, idAgence, idProprietaire, idSyndic)
-Values (1, '5 rue de l alsace, 69003 Lyon', 1, 385, 25, 16, 1, 10, 1);
+INSERT INTO BIEN (idBien, idAgence, idPropri, idSyndic, adresse, typeBail, Loyer, surface)
+Values (2,  3, 3, 3, 'Paris, France', 3, 385, 25);
 
+INSERT INTO BIEN (idBien, idAgence, idPropri, idSyndic, adresse, typeBail, Loyer, surface)
+Values (3,  4, 4, 4, 'La Rochelle, France', 3, 385, 25);
 
--- Bien n°2 :
-INSERT INTO BIEN (idBien, adrBien, bail, loyer, surface, nbrVisite, idAgence, idProprietaire, idSyndic)
-Values (2, '7 rue de l alsace, 69003 Lyon', 3, 485, 57, 16, 1, 20, 1);
+INSERT INTO BIEN (idBien, idAgence, idPropri, idSyndic, adresse, typeBail, Loyer, surface)
+Values (4,  5, 5, 5, 'Rochefort, France', 3, 385, 25);
 
--- Bien n°3
-INSERT INTO BIEN (idBien, adrBien, bail, loyer, surface, nbrVisite, idAgence, idProprietaire, idSyndic)
-Values (3, '9 rue Duguesclin, 59800 Lille', 3, 600, 200, 34, 2, 30,2);
+-- Insertion d'une LOCATION
 
+INSERT INTO LOCATION (idLocation, dateDebut, datePrevFin, dateFin, idLocataire, idBien)
+VALUES (1, to_date('20-11-2022', 'dd-mm-yyyy'), to_date('19-11-2025', 'dd-mm-yyyy'), 2, 2);
 
--- Insertion de locations :
+-- Insertion d'une Visite
 
--- Location n°1 :
-INSERT INTO LOCATION (idLocation, dateDebut, datePrevFin, idLocataire, idBien)
-VALUES (1, to_date('20-11-2022', 'dd-mm-yyyy'), to_date('19-11-2025', 'dd-mm-yyyy'), 11, 3);
+INSERT INTO VISITE (idVisite, idBien, dateVisite, Heure, Retour)
+VALUES(1, 3, to_date('23/11/2022', 'DD/MM/YYYY'), 14, 'Manque d eclerage');
 
--- location n°2 :
-INSERT INTO LOCATION (idLocation, dateDebut, datePrevFin, idLocataire, idBien)
-VALUES (2, to_date('24-11-2022', 'dd-mm-yyyy'), to_date('23-11-2025', 'dd-mm-yyyy'), 22, 2);
+-- Insertion d'un DPE
 
-spool off;
+INSERT INTO DPE (idDPE, idBien, dateDebut, dateFin, classeEnergie, classeClimat, consommation, emmissions)
+VALUES(1, 4, to_date('23/12/2011', 'DD/MM/YYYY'), to_date('22/12/2021', 'DD/MM/YYYY'), 'C', 'C', 147, 20);
